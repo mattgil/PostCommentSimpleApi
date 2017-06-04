409e7533c5871a7f7cc839f742f6593c948ccd3c
mateusz <mateusz.gil@artlpus.pl>
2017-06-04 16:00:22 +0200

    index route redirect to doc

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mateusz
+ * Date: 04.06.17
+ * Time: 15:57
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction() {
+        return new RedirectResponse("/doc");
+    }
+}
